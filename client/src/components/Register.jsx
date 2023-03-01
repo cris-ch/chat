f01fb6e3e54de7d89da5e6cb64e33b5713ca7d21
Cristian <cristian.christensen@gmail.com>
2023-03-01 18:23:12 +0100

    First commit

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="bg-blue-50 h-screen flex items-center">
+      <form className="w-64 mx-auto mb-12">
+        <input
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          type="text"
+          placeholder="Username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <button
+          type="submit"
+          className="block bg-blue-500 text-white w-full rounded-sm p-2"
+        >
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
